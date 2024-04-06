d6892a7c6085449456b87d8f098ccacb5769d158
aoreo <hay6586@gmail.com>
2024-04-06 14:10:53 +0900

    :lipstick: Add Splash Screen

---
+++ app/src/main/java/com/ajou/xive/SplashActivity.kt
@@ -0,0 +1,64 @@
+package com.ajou.xive
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import com.ajou.xive.auth.SignUpActivity
+import com.ajou.xive.databinding.ActivitySplashBinding
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+    private var _binding: ActivitySplashBinding? = null
+    private val binding get() = _binding!!
+    private var accessToken : String ?= null
+    private var refreshToken : String ?= null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val gifDrawable = pl.droidsonroids.gif.GifDrawable(resources, R.drawable.splash_logo)
+        binding.logo.setImageDrawable(gifDrawable)
+        gifDrawable.setSpeed(0.8f)
+
+        Handler().postDelayed({
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        },2000)
+
+        CoroutineScope(Dispatchers.IO).launch(exceptionHandler) {
+//            if (accessToken == null || refreshToken == null){
+//
+//            }
+            // TODO 서버와 연결 후에는 token 여부와 firstFlag로 식별하기
+        }
+    }
+
+    val exceptionHandler = CoroutineExceptionHandler { _, exception ->
+        Log.d("exceptionHandler", exception.message.toString())
+        showErrorDialog()
+    }
+    private fun showErrorDialog() {
+        runOnUiThread {
+            AlertDialog.Builder(this).apply {
+                setTitle("Error")
+                setMessage("Network request failed.")
+                setPositiveButton("종료") { dialog, _ ->
+                    dialog.dismiss()
+                    finish()
+                }
+                setCancelable(false)
+                show()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/ajou/xive/auth/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.ajou.xive.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ajou.xive.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
